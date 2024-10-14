e63e6394c2bb5f86d0c8df3506550c493baaa95f
fadilahmuh <67127698+fadilahmuh@users.noreply.github.com>
2024-10-14 13:28:20 +0700

    update

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -47,7 +47,10 @@
           <li class="nav-item dropdown d-none d-lg-block user-dropdown">
             <a class="nav-link" id="UserDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
               {{-- <img class="img-xs rounded-circle" src="{{$user->img}}" alt="Profile image"> </a> --}}
-              <img class="img-xs rounded-circle" src="{{ Auth::user()->img }}" alt="Profile image"> </a>
+              {{-- <img class="img-xs rounded-circle" loading="eager" src="{{ Auth::user()->img }}" alt="Profile image"> </a> --}}
+              <div id="avatar">
+
+              </div>
             <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
               <div class="dropdown-header text-center">
                 {{-- <p class="mb-1 mt-3 font-weight-semibold">{{$user->name}}</p> --}}
@@ -162,22 +165,28 @@
   <script src="assets/vendors/js/vendor.bundle.base.js"></script>
   <!-- endinject -->
   <!-- Plugin js for this page -->
-  <script src="assets/vendors/chart.js/Chart.min.js"></script>
+  {{-- <script src="assets/vendors/chart.js/Chart.min.js"></script>
   <script src="assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
-  <script src="assets/vendors/progressbar.js/progressbar.min.js"></script>
+  <script src="assets/vendors/progressbar.js/progressbar.min.js"></script> --}}
 
   <!-- End plugin js for this page -->
   <!-- inject:js -->
   <script src="assets/js/off-canvas.js"></script>
-  <script src="assets/js/hoverable-collapse.js"></script>
+  <script>
+      const profileImage = new Image();
+      profileImage.src = "{{ Auth::user()->img }}";
+      profileImage.className = "img-xs rounded-circle";
+      document.getElementById("avatar").appendChild(profileImage);
+  </script>
+  {{-- <script src="assets/js/hoverable-collapse.js"></script>
   <script src="assets/js/template.js"></script>
   <script src="assets/js/settings.js"></script>
-  <script src="assets/js/todolist.js"></script>
+  <script src="assets/js/todolist.js"></script> --}}
   <!-- endinject -->
   <!-- Custom js for this page-->
-  <script src="assets/js/jquery.cookie.js" type="text/javascript"></script>
+  {{-- <script src="assets/js/jquery.cookie.js" type="text/javascript"></script>
   <script src="assets/js/dashboard.js"></script>
-  <script src="assets/js/Chart.roundedBarCharts.js"></script>
+  <script src="assets/js/Chart.roundedBarCharts.js"></script> --}}
   <!-- End custom js for this page-->
 </body>
 
--- resources/views/player/1.blade.php
+++ resources/views/player/1.blade.php
@@ -50,36 +50,52 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js"></script>
     <script>
-        $.ajax({
-                method: "GET",
-                url: "https://api.spotify.com/v1/me/player/currently-playing",
-                headers: {
-                "Content-Type":    "application/json",
-                "Authorization":   "Bearer {{$user->access_token}}",
-                },
-                success: function(result) {
-                    $('.artist').text(result.item.album.artists[0].name);
-                    $('.cover').css('background-image', 'url(' + result.item.album.images[1].url + ')');
-                    $('.song').text(result.item.name);
-                    $('.duration').text(convert(result.progress_ms) +'/'+ convert(result.item.duration_ms));
-                    $(':root').css('--progress-bar', bars(result.progress_ms,result.item.duration_ms)+'%' );
-                },
-                error: function(xhr, ajaxOptions, thrownError) {
-                    console.log(xhr.status + "\n" + xhr.responseText + "\n" + thrownError);
-                }
-                
-            });
-
+        
+        let run = async () => {
+            while (true) {
+                $.ajax({
+                    method: "GET",
+                    url: "https://api.spotify.com/v1/me/player/currently-playing",
+                    headers: {
+                    "Content-Type":    "application/json",
+                    "Authorization":   "{{$user->access_token}}",
+                    },
+                    success: function(result) {
+                        let artist = [];
+                        for (let index = 0; index < result.item.artists.length; index++) {                   
+                            artist.push(result.item.artists[index].name)
+                        }
+                        $('.artist').text(artist.join());
+                        $('.cover').css('background-image', 'url(' + result.item.album.images[1].url + ')');
+                        $('.song').text(result.item.name);
+                        $('.duration').text(convert(result.progress_ms) +'/'+ convert(result.item.duration_ms));
+                        $(':root').css('--progress-bar', bars(result.progress_ms,result.item.duration_ms)+'%' );
+                    },
+                    error: function(xhr, ajaxOptions, thrownError) {
+                        console.log(xhr.status + "\n" + xhr.responseText + "\n" + thrownError);
+                    }        
+                });
+                await sleep(1000)
+            }
+        }
             
         function convert(millis) {
-        var minutes = Math.floor(millis / 60000);
-        var seconds = ((millis % 60000) / 1000).toFixed(0);
-        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+            var minutes = Math.floor(millis / 60000);
+            var seconds = ((millis % 60000) / 1000).toFixed(0);
+            return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
         }
 
         function bars(prog, dur) {
-        var percent = (prog / dur * 100);
-        return percent;
+            var percent = (prog / dur * 100);
+            return percent;
         }
+
+        const sleep = (delay) => {
+            return new Promise(function(resolve) {
+                setTimeout(resolve, delay);
+            });
+        }
+
+        run();
     </script>
 </html>
